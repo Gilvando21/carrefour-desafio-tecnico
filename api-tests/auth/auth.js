de68fac6c75121485263bcc9cf847cddaee7f402
GilvandoSenaMatos <gilvando0321@gmail.com>
2026-03-15 14:00:07 -0300

    Add gitignore to ignore node_modules and artifacts

--- api-tests/auth/auth.js
+++ api-tests/auth/auth.js
@@ -1,14 +1,27 @@
-
 const request = require("supertest")
 
 const BASE_URL = "https://serverest.dev"
 
 async function getToken(){
+
+  const email = `qa${Date.now()}@test.com`
+
+  // cria usuário
+  await request(BASE_URL)
+    .post("/usuarios")
+    .send({
+      nome: "QA Automation",
+      email: email,
+      password: "123456",
+      administrador: "true"
+    })
+
+  // faz login
   const res = await request(BASE_URL)
     .post("/login")
     .send({
-      email:"admin@admin.com",
-      password:"admin"
+      email: email,
+      password: "123456"
     })
 
   return res.body.authorization
